8da605d39c5c21d0edd1e996cba568ef2df47d74
MaxTsai <maxtsai@sosreader.com>
2021-02-07 20:29:18 +0800

    rm a tag

--- src/App.js
+++ src/App.js
@@ -4,17 +4,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <h1>Hello world afte action setting</h1>
         <h2>Second change</h2>
         <h3>Third change</h3>
